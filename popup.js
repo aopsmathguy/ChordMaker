699523f1f1553dfd44e3c926a3faadb5c6d6da50
matthew <matzhao@gmail.com>
2025-02-16 18:31:56 -0500

    themes

--- popup.js
+++ popup.js
@@ -1,3 +1,47 @@
+
+const columns = document.getElementById('columns');
+const maxWidth = document.getElementById('maxWidth');
+const theme0 = document.getElementById('theme0');
+const theme1 = document.getElementById('theme1');
+const theme2 = document.getElementById('theme2');
+document.addEventListener('DOMContentLoaded', () => {
+  
+    // Load saved settings (if any)
+    columns.value = localStorage.getItem('columns') || 2;
+    maxWidth.value = localStorage.getItem('maxWidth') || 50;
+    theme0.value = localStorage.getItem('theme0') || "#ffffff";
+    theme1.value = localStorage.getItem('theme1') || "#000000";
+    theme2.value = localStorage.getItem('theme2') || "#ff0000";  
+  
+    // Listen for changes and save to localStorage
+    columns.addEventListener('change', () => {
+      localStorage.setItem('columns', columns.value);
+    });
+  
+    maxWidth.addEventListener('change', () => {
+      localStorage.setItem('maxWidth', maxWidth.value);
+    });
+
+    theme0.addEventListener('change', () => {
+        localStorage.setItem('theme0', theme0.value);
+    });
+    theme1.addEventListener('change', () => {
+        localStorage.setItem('theme1', theme1.value);
+    });
+    theme2.addEventListener('change', () => {
+        localStorage.setItem('theme2', theme2.value);
+    });
+});
+function hexToRgb(hex) {
+    hex = hex.replace(/^#/, '');
+    let bigint = parseInt(hex, 16);
+    return [
+        (bigint >> 16) & 255,
+        (bigint >> 8) & 255,
+        bigint & 255
+    ]
+}
+
 function sendMessage(action, data) {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         if (tabs.length === 0) {
@@ -19,14 +63,7 @@ document.getElementById('increase').addEventListener('click', () => {
 document.getElementById('decrease').addEventListener('click', () => {
     sendMessage('decrease');
 });
-let columns = document.getElementById('columns').value;
-let maxWidth = document.getElementById('maxWidth').value;
-document.getElementById('columns').addEventListener('change', (event) => {
-    columns = event.target.value;
-});
-document.getElementById('maxWidth').addEventListener('change', (event) => {
-    maxWidth = event.target.value;
-});
 document.getElementById('download').addEventListener('click', () => {
-    sendMessage('download', { columns, maxWidth });
+    const theme = [hexToRgb(theme0.value), hexToRgb(theme1.value), hexToRgb(theme2.value)];
+    sendMessage('download', { columns: columns.value, maxWidth : maxWidth.value, theme: theme });
 });
